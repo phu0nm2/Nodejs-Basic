8e7de8250ebea6f26f1389e59702d2acf8e55374
Thanh Nguyen <phu0nm2@gmail.com>
2023-04-01 16:19:09 +0700

    view engine, static files

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+require("dotenv").config();
+
 const app = require("./app");
+const configViewEngine = require("./src/config.js/viewEngine");
 
 const http = require("http");
 
 const server = http.createServer(app);
 
 const port = process.env.PORT || 8000;
+
+configViewEngine(app);
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
 server.listen(port, () => {
   console.log(`server running on port ${port}`);
