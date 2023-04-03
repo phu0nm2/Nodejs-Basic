962f53d767495f4507daa411d508ac8893cf5f9e
Thanh Nguyen <phu0nm2@gmail.com>
2023-04-03 10:03:23 +0700

    Connect MySQL

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,15 @@
+const connection = require("../config.js/configDB");
+
 exports.homePage = (req, res) => {
-  return res.render("index.ejs");
+  // simple query
+  connection.query("SELECT * FROM `users`", function (err, results, fields) {
+    console.log(results); // results contains rows returned by server
+
+    const data = results.map((row) => {
+      return row;
+    });
+
+    console.log("data", typeof data);
+    return res.render("index.ejs", { dataUser: JSON.stringify(data) });
+  });
 };
